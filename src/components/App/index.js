627228fa123cb2b11285524c0074c70573a600cb
Caspar Jeffrey <Caspar.Jeffrey@arup.com>
2018-09-11 17:50:10 +1000

    messy wip

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        sup
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,null
+)(App)
